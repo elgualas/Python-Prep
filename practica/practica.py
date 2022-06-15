d0053eb78e83e86c292cde0a4f477654f6626dc0
elgualas <drgualas@gmail.com>
2022-06-15 12:47:51 -0500

    ejercicios

--- practica/practica.py
+++ practica/practica.py
@@ -12,12 +12,39 @@ while n<len(a):
     if a[n]%2==0:
         print(a[n])
     n+=1
-"""
+
 a=[-15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1]
 
 for i in a:
     if i%2==0:
         print(i)
-        
+
+a=[-15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1]
+for i in a[0:3]:
+    print(i)
+
+a=[-15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1]
+
+for i,e in enumerate(a):
+    print(i,e)
+
+lista = [1,2,5,7,8,10,13,14,15,17,20]
+n=1
+while n<=20:
+    if not(n in lista):
+        lista.insert(n-1,n)
+    n+=1
+print(lista)  
+"""
+lista=[0,1]
+c=2
+while c<=30:
+    for i in lista:
+        lista.append(i+(i+1))
+    c+=1
+print(lista)
+    
+
+
 
 
