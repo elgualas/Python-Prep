98f4356932abaf5c51662e0d1d7f8f21697ced20
unknown <drgualas@gmail.com>
2022-06-18 18:24:13 -0500

    funciones

--- practica/funciones.py
+++ practica/funciones.py
@@ -23,7 +23,19 @@ def lis_frec(a):
         return None
     for elementos in a:
         if elementos in list_un:
-            return list_un
-        
+            i=list_un.index(elementos)
+            list_mod[i]+=1
+        else:
+            list_un.append(elementos)
+            list_mod.append(1)
+    moda=list_un[0]
+    rep=list_mod[0]
+    print(list_un)
+    print(list_mod)
+    for i,elementos in enumerate(list_un):
+        if list_mod[i]>rep:
+            moda=list_un[i]
+            rep=list_mod[i]
+    return moda, rep
 
-print(lis_frec([2,5,3,2,6,2,8,2,9,7]))
+print(lis_frec([5,3,2,6,2,3,3,3,3,8,2,2,9,7]))
